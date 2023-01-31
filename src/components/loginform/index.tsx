37317ffa54e4463c899ed929cd4850b2f186de17
Erisnilton Freitas <erisniltongf@gmail.com>
2023-01-31 14:06:45 -0300

    fix: validation fields with schema

--- src/components/loginform/index.tsx
+++ src/components/loginform/index.tsx
@@ -4,6 +4,8 @@ import "./styles.scss";
 import Button from "../button";
 
 import { Link, useNavigate } from "react-router-dom";
+import { LoginSchema } from "../../lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type Inputs = {
   email: string;
@@ -19,8 +21,8 @@ const LoginForm: React.FunctionComponent = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = ({ email, password }) => {
+  } = useForm<LoginSchema>({ resolver: zodResolver(LoginSchema) });
+  const onSubmit: SubmitHandler<LoginSchema> = ({ email, password }) => {
     setEmail(email);
     setPassword(password);
     navigate("/");
@@ -35,18 +37,14 @@ const LoginForm: React.FunctionComponent = () => {
         <div className="container__right-login">
           <h1>Faça Seu Login</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              {...register("email", { required: true })}
-              placeholder="Email"
-              autoFocus
-            />
-            {errors.email && <span>O Campo Email é Obrigatório!</span>}
+            <input {...register("email")} placeholder="Email" autoFocus />
+            {errors.email && <span>{errors.email.message}</span>}
             <input
               type={"password"}
               {...register("password", { required: true })}
               placeholder="Senha"
             />
-            {errors.password && <span>O Campo senha é Obrigatório!</span>}
+            {errors.password && <span>{errors.password.message}</span>}
             <Button color="primary" size="lg">
               Fazer Login
             </Button>

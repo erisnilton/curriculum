f81572020682798f2ff9dd0dd676adde7a8f20b8
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-01 10:09:25 -0300

    fix: alter message error password

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const LoginSchema = z.object({
   email: z
     .string()
-    .min(1, { message: "O Email é obrigatório." })
+    .min(1, { message: "O email é obrigatório." })
     .email({ message: "Digite um email válido." }),
-  password: z.string().min(1, { message: "A Senha é obrigatório." }),
+  password: z.string().min(6, { message: "A senha deve ter no minimo 6 caracteres." }),
 });
 
 export type LoginSchema = z.infer<typeof LoginSchema>;

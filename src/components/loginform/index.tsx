63fe797234aea7f784b2b906fd4e1a43977edaa7
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-01 10:10:51 -0300

    fix: using custom component input

--- src/components/loginform/index.tsx
+++ src/components/loginform/index.tsx
@@ -6,49 +6,50 @@ import Button from "../button";
 import { Link, useNavigate } from "react-router-dom";
 import { LoginSchema } from "../../lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-type Inputs = {
-  email: string;
-  password: string;
-};
+import { Input } from "../input";
 
 const LoginForm: React.FunctionComponent = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginSchema>({ resolver: zodResolver(LoginSchema) });
+
   const onSubmit: SubmitHandler<LoginSchema> = ({ email, password }) => {
-    setEmail(email);
-    setPassword(password);
+    if (email === "" || password === "") return;
     navigate("/");
   };
   return (
     <div className="container">
       <div className="container__left">
-        <h1>Faça Seu Login</h1>
+        <h1>Faça Login</h1>
       </div>
 
       <div className="container__right">
         <div className="container__right-login">
-          <h1>Faça Seu Login</h1>
+          <h1>Faça Login</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email")} placeholder="Email" autoFocus />
-            {errors.email && <span>{errors.email.message}</span>}
-            <input
-              type={"password"}
-              {...register("password", { required: true })}
-              placeholder="Senha"
-            />
-            {errors.password && <span>{errors.password.message}</span>}
-            <Button color="primary" size="lg">
-              Fazer Login
-            </Button>
-            <Link to={""}>Criar Conta</Link>
+            <div>
+              <Input
+                placeholder="Email"
+                name="email"
+                register={{ ...register("email") }}
+                error={errors.email?.message}
+              />
+              <Input
+                placeholder="Senha"
+                name="password"
+                type="password"
+                register={{ ...register("password") }}
+                error={errors.password?.message}
+              />
+            </div>
+              <Button color="primary" size="lg">
+                Fazer Login
+              </Button>
+              <Link to={""}>Criar Conta</Link>
           </form>
         </div>
       </div>

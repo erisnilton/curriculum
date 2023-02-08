6b319492493db3869c26bbf41ab83b150ecc22d9
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-08 09:11:00 -0300

    fix: adjust about form

--- src/components/about-form/index.tsx
+++ src/components/about-form/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
-import "./styles.scss";
 
 import { useNavigate } from "react-router-dom";
 import { AboutSchema } from "../../lib/validation";
@@ -9,28 +8,48 @@ import { Input } from "../input";
 import Button from "../button";
 import TextArea from "../textarea";
 
+import "./styles.scss";
+
 const AboutForm: React.FunctionComponent = () => {
   const navigate = useNavigate();
 
   const methods = useForm<AboutSchema>({ resolver: zodResolver(AboutSchema) });
 
   const onSubmit: SubmitHandler<AboutSchema> = () => {};
+
+  const handleReset = () => {
+    methods.reset();
+  };
   return (
-    <div className="container">
+    <div className="about__form">
+      <h1>Sobre</h1>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <div>
-            <Input placeholder="Nome" name="name" />
-            <Input placeholder="Sobrenome" name="surname" />
-            <Input placeholder="Título" name="title" />
-            <Input placeholder="Email" name="email" />
-            <Input placeholder="Endereço" name="address" />
-            <TextArea name="about" placeholder="Sobre" />
+            <div>
+              <Input label="Nome" name="name" />
+              <Input label="Sobrenome" name="surname" />
+              <Input label="Endereço" name="address" />
+            </div>
+            <div>
+              <Input label="Título" name="title" />
+              <Input label="Email" name="email" type="email" />
+            </div>
+            <Input label="Imagem" name="image" />
+            <Input label="Curriculo Lattes" name="lattes" type="text" />
+            <Input label="Instagran" name="instagran" type="text" />
+            <Input label="Linkedin" name="linkedin" type="text" />
+            <Input label="Github" name="github" type="text" />
+            <TextArea label="Sobre" name="about" />
           </div>
-
-          <Button color="primary" size="md">
-            OK
-          </Button>
+          <div className="d-flex">
+            <Button color="primary" size="md">
+              Salvar e continuar
+            </Button>
+            <Button color="danger" size="md" onClick={handleReset}>
+              Cancelar
+            </Button>
+          </div>
         </form>
       </FormProvider>
     </div>

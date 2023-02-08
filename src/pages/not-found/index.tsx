246f0f29cd3c99475fbac3c18620eead652d360f
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-08 09:32:51 -0300

    feat: create page notfound

---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+
+const NotFound: React.FunctionComponent = () => {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Página não encontrada</h2>
+    </div>
+  );
+};
+
+export default NotFound;

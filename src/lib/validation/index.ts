c35f18e516a7b0d5c21bf019c489daaa9b1c9da6
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-08 08:59:09 -0300

    feat: create validation schema

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,10 +1,10 @@
+import { Education } from './../../pages/education/index';
 import { z } from "zod";
 
 export const LoginSchema = z.object({
-  email: z
+  registration: z
     .string()
-    .min(1, { message: "O email é obrigatório." })
-    .email({ message: "Digite um email válido." }),
+    .min(1, { message: "A matricula é obrigatória." }),
   password: z
     .string()
     .min(6, { message: "A senha deve ter no minimo 6 caracteres." }),
@@ -27,3 +27,27 @@ export const AboutSchema = z.object({
 });
 
 export type AboutSchema = z.infer<typeof AboutSchema>;
+
+
+export const EducationSchema = z.object({
+  institution: z.string().min(1, { message: "A instituição é obrigatória." }),
+  course: z.string().min(1, { message: "O curso é obrigatório." }),
+  start_date: z.string().min(1, { message: "A data de início é obrigatória." }),
+  end_date: z.string().min(1, { message: "A data de término é obrigatória." }),
+  description: z.string().optional(),
+
+});
+
+export type EducationSchema = z.infer<typeof EducationSchema>;
+
+export const ExperienceSchema = z.object({
+  title: z.string().min(1, { message: "O título é obrigatório." }),
+  intitution: z.string().min(1, { message: "A instituição é obrigatória." }),
+  start_date: z.string().min(1, { message: "A data de início é obrigatória." }),
+  end_date: z.string().min(1, { message: "A data de término é obrigatória." }),
+  description: z.string().optional(),
+});
+
+export type ExperienceSchema = z.infer<typeof ExperienceSchema>;
+
+

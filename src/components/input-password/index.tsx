36aa7f654c52f619a70716a75dbc1b679ddf3c9b
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-08 09:30:25 -0300

    feat: create component input-password

---
+++ src/components/input-password/index.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Input } from "../input";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import "./styles.scss";
+
+export const InputPassword: React.FunctionComponent = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="input__password">
+      <Input
+        label="Senha"
+        name="password"
+        type={showPassword ? "text" : "password"}
+      />
+      <div className="input__password--icon" onClick={() => setShowPassword(!showPassword)}>
+        {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+      </div>
+    </div>
+  );
+};
+
+export default InputPassword;

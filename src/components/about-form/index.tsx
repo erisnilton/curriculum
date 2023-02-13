90fff5b5e2340e0603a0c46564f6eba483b82b50
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-13 15:51:35 -0300

    chore: adjust form

--- src/components/about-form/index.tsx
+++ src/components/about-form/index.tsx
@@ -4,37 +4,33 @@ import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { AboutSchema } from "../../lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "../input";
-import Button from "../button";
 import TextArea from "../textarea";
-
+import { Input } from "../input";
+import { useModal } from "../modal";
 import "./styles.scss";
+import Button from "../button";
 
 const AboutForm: React.FunctionComponent = () => {
-  const navigate = useNavigate();
-
   const methods = useForm<AboutSchema>({ resolver: zodResolver(AboutSchema) });
 
-  const onSubmit: SubmitHandler<AboutSchema> = () => {};
-
-  const handleReset = () => {
-    methods.reset();
+  const onSubmit: SubmitHandler<AboutSchema> = (values) => {
+    console.log(values);
+    modal.close();
   };
+
+  const modal = useModal();
   return (
     <div className="about__form">
-      <h1>Sobre</h1>
+      <h2>Sobre</h2>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <div>
-            <div>
-              <Input label="Nome" name="name" />
-              <Input label="Sobrenome" name="surname" />
-              <Input label="Endereço" name="address" />
-            </div>
-            <div>
-              <Input label="Título" name="title" />
-              <Input label="Email" name="email" type="email" />
-            </div>
+            <Input label="Nome" name="name" />
+            <Input label="Sobrenome" name="surname" />
+            <Input label="Endereço" name="address" />
+            <Input label="Título" name="title" />
+            <Input label="Email" name="email" type="email" />
+            <Input label="Telefone" name="phone" />
             <Input label="Imagem" name="image" />
             <Input label="Curriculo Lattes" name="lattes" type="text" />
             <Input label="Instagran" name="instagran" type="text" />
@@ -42,13 +38,11 @@ const AboutForm: React.FunctionComponent = () => {
             <Input label="Github" name="github" type="text" />
             <TextArea label="Sobre" name="about" />
           </div>
-          <div className="d-flex">
-            <Button color="primary" size="md">
-              Salvar e continuar
-            </Button>
-            <Button color="danger" size="md" onClick={handleReset}>
+          <div style={{textAlign: 'end'}}>
+            <Button color="secondary" onClick={modal.close}>
               Cancelar
             </Button>
+            <Button color="primary">Salvar</Button>
           </div>
         </form>
       </FormProvider>

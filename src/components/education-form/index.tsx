2e2b6cf800c6edf1b7897e0474aecc002e8409c7
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-13 15:52:03 -0300

    chore: adjust form education

--- src/components/education-form/index.tsx
+++ src/components/education-form/index.tsx
@@ -1,29 +1,27 @@
 import react from "react";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { EducationSchema } from "../../lib/validation";
-import { zodResolver } from "@hookform/resolvers/zod";  
-import Button from "../button";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../input";
 import TextArea from "../textarea";
-
 import "./styles.scss";
+import { useModal } from "../modal";
+import Button from "../button";
 
 const EducationForm: React.FunctionComponent = () => {
   const methods = useForm<EducationSchema>({
     resolver: zodResolver(EducationSchema),
   });
 
-  const onSubmit = () => {
-    console.log("submit");
-  };
+  const modal = useModal();
 
-  const handleReset = () => {
-    methods.reset();
+  const onSubmit = () => {
+    modal.close();
   };
 
   return (
     <div className="education__form">
-      <h1>Formação Educacional</h1>
+      <h2>Formação Educacional</h2>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <Input label="Instituição de ensino" name="institution" />
@@ -31,14 +29,11 @@ const EducationForm: React.FunctionComponent = () => {
           <Input label="Data de Início" name="start_date" />
           <Input label="Data de Termino" name="end_date" />
           <TextArea label="Descrição" name="description" />
-
-          <div className="d-flex">
-            <Button color="primary" size="md">
-              Salvar e continuar
-            </Button>
-            <Button color="danger" size="md" onClick={handleReset}>
+          <div style={{ textAlign: "end" }}>
+            <Button color="secondary" onClick={modal.close}>
               Cancelar
             </Button>
+            <Button color="primary">Salvar</Button>
           </div>
         </form>
       </FormProvider>

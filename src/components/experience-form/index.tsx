002cd07e73224834a3546935421d9ed1c6c27a68
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-13 15:52:25 -0300

    chore: adjust form experience

--- src/components/experience-form/index.tsx
+++ src/components/experience-form/index.tsx
@@ -1,29 +1,28 @@
 import React from "react";
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import { ExperienceSchema } from "../../lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Button from "../button";
 
 import "./styles.scss";
 import { Input } from "../input";
 import TextArea from "../textarea";
+import { useModal } from "../modal";
+import Button from "../button";
 
 const ExperienceForm: React.FunctionComponent = () => {
   const methods = useForm<ExperienceSchema>({
     resolver: zodResolver(ExperienceSchema),
   });
 
-  const onSubmit = () => {
-    console.log("submit");
-  };
+  const modal = useModal();
 
-  const handleReset = () => {
-    methods.reset();
+  const onSubmit = () => {
+    modal.close();
   };
 
   return (
     <div className="experience__form">
-      <h1>Experiência Profissional</h1>
+      <h2>Experiência Profissional</h2>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <Input label="Titulo" name="title" />
@@ -31,14 +30,11 @@ const ExperienceForm: React.FunctionComponent = () => {
           <Input label="Data de Início" name="start_date" />
           <Input label="Data de Termino" name="end_date" />
           <TextArea label="Descrição" name="description" />
-
-          <div className="d-flex">
-            <Button color="primary" size="md">
-              Finalizar
+          <div style={{ textAlign: "end" }}>
+            <Button color="secondary" onClick={modal.close}>
+              Cancelar
             </Button>
-            <Button color="danger" size="md" onClick={handleReset}>
-              Voltar
-            </Button>
+            <Button color="primary">Salvar</Button>
           </div>
         </form>
       </FormProvider>

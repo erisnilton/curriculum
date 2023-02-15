f1386162b07f659375fa18d621eb4009c0300eb1
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-15 15:21:20 -0300

    chore: improve layout about form

--- src/components/about-form/index.tsx
+++ src/components/about-form/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
-
-import { useNavigate } from "react-router-dom";
 import { AboutSchema } from "../../lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import TextArea from "../textarea";
@@ -9,14 +7,51 @@ import { Input } from "../input";
 import { useModal } from "../modal";
 import "./styles.scss";
 import Button from "../button";
+import { UpdateAbout, CreateAbout } from "../../backend";
+import { toast } from "react-toastify";
 
-const AboutForm: React.FunctionComponent = () => {
+
+import { useAboutContext } from "../../contexts/about-context";
+
+export interface AboutFormProps {
+  edit?: boolean;
+}
+
+const AboutForm: React.FunctionComponent<AboutFormProps> = ({ edit }) => {
   const methods = useForm<AboutSchema>({ resolver: zodResolver(AboutSchema) });
+  const { items, loadItems } = useAboutContext();
 
-  const onSubmit: SubmitHandler<AboutSchema> = (values) => {
-    console.log(values);
+  const onSubmit: SubmitHandler<AboutSchema> = async (values) => {
+    if (edit) {
+      await UpdateAbout(values, 1);
+      toast.success("Informações Atualizada com sucesso!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } else {
+      await CreateAbout(values);
+      toast.success("Informações Cadastrada com sucesso!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
     modal.close();
+    loadItems();
   };
+
+  useEffect(() => {
+    if (edit) {
+      methods.setValue("fullname", items.fullname);
+      methods.setValue("address", items.address);
+      methods.setValue("title", items.title);
+      methods.setValue("email", items.email);
+      methods.setValue("phone", items.phone);
+      methods.setValue("url_image", items.url_image);
+      methods.setValue("url_lattes", items.url_lattes);
+      methods.setValue("url_instagram", items.url_instagram);
+      methods.setValue("url_linkedin", items.url_linkedin);
+      methods.setValue("url_github", items.url_github);
+      methods.setValue("about", items.about);
+    }
+  }, [edit]);
 
   const modal = useModal();
   return (
@@ -24,21 +59,40 @@ const AboutForm: React.FunctionComponent = () => {
       <h2>Sobre</h2>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <div>
-            <Input label="Nome" name="name" />
-            <Input label="Sobrenome" name="surname" />
+          <div className="about__form--input">
+            <Input label="Nome completo" name="fullname" />
+          </div>
+          <div className="about__form--input">
             <Input label="Endereço" name="address" />
+          </div>
+          <div className="about__form--input">
             <Input label="Título" name="title" />
+          </div>
+          <div className="about__form--input">
             <Input label="Email" name="email" type="email" />
+          </div>
+          <div className="about__form--input">
             <Input label="Telefone" name="phone" />
-            <Input label="Imagem" name="image" />
-            <Input label="Curriculo Lattes" name="lattes" type="text" />
-            <Input label="Instagran" name="instagran" type="text" />
-            <Input label="Linkedin" name="linkedin" type="text" />
-            <Input label="Github" name="github" type="text" />
+          </div>
+          <div className="about__form--input">
+            <Input label="Imagem" name="url_image" />
+          </div>
+          <div className="about__form--input">
+            <Input label="Curriculo Lattes" name="url_lattes" />
+          </div>
+          <div className="about__form--input">
+            <Input label="Instagran" name="url_instagram" />
+          </div>
+          <div className="about__form--input">
+            <Input label="Linkedin" name="url_linkedin" />
+          </div>
+          <div className="about__form--input">
+            <Input label="Github" name="url_github" />
+          </div>
+          <div className="about__form--input">
             <TextArea label="Sobre" name="about" />
           </div>
-          <div style={{textAlign: 'end'}}>
+          <div className="about__form--action">
             <Button color="secondary" onClick={modal.close}>
               Cancelar
             </Button>

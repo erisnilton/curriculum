4bb9971c9912f8399f505154ed1ae4fc54fcaefa
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-15 15:18:15 -0300

    chore: configuration toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import AppRoutes from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />;
+      <ToastContainer autoClose={3000} />
+    </>
+  );
 }
 
 export default App;

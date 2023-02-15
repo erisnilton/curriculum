4c0750af28f3718d5edfeaf0ed389f0fc97f9c1e
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-15 15:28:33 -0300

    chore: adjustment routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Account from "../pages/account";
 import Login from "../pages/login";
 import { Main } from "../pages/main";
 import NotFound from "../pages/not-found";
@@ -10,6 +11,7 @@ const AppRoutes: React.FunctionComponent = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="login" element={<Login />} />
+        <Route path="account" element={<Account />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

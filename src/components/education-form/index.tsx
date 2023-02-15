0a67ca741dbfe2e7f537ae055a62b7a7fa8538a0
Erisnilton Freitas <erisniltongf@gmail.com>
2023-02-15 15:22:52 -0300

    chore: improve layout education form

--- src/components/education-form/index.tsx
+++ src/components/education-form/index.tsx
@@ -1,22 +1,58 @@
-import react from "react";
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import { EducationSchema } from "../../lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "../input";
 import TextArea from "../textarea";
 import "./styles.scss";
 import { useModal } from "../modal";
 import Button from "../button";
+import { useEducationContext } from "../../contexts/education-context";
+import { Input } from "../input";
+import react, { useEffect } from "react";
+import { CreateEducation, GetEducationById, UpdateEducation } from "../../backend";
+import { toast } from "react-toastify";
 
-const EducationForm: React.FunctionComponent = () => {
+export interface EducationFormProps {
+  edit?: boolean;
+  id?: number
+}
+
+const EducationForm: React.FunctionComponent<EducationFormProps> = ({
+  edit,
+  id,
+}) => {
   const methods = useForm<EducationSchema>({
     resolver: zodResolver(EducationSchema),
   });
 
   const modal = useModal();
+  const { loadItems } = useEducationContext();
 
-  const onSubmit = () => {
+  useEffect(() => {
+    if (edit && id) {
+      GetEducationById(id).then((item) => {
+        methods.setValue("institution", item.institution);
+        methods.setValue("course", item.course);
+        methods.setValue("start_date", item.start_date);
+        methods.setValue("end_date", item.end_date);
+        methods.setValue("description", item.description);
+      });
+    }
+  }, [edit, id]);
+
+  const onSubmit = async (values: EducationSchema) => {
+    if (edit) {
+      await UpdateEducation(values, id);
+      toast.success("Informação atualizada com sucesso!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } else {
+      await CreateEducation(values);
+      toast.success("Informação adicionada com sucesso!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
     modal.close();
+    loadItems();
   };
 
   return (
@@ -24,12 +60,23 @@ const EducationForm: React.FunctionComponent = () => {
       <h2>Formação Educacional</h2>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <Input label="Instituição de ensino" name="institution" />
-          <Input label="Cursos" name="course" />
-          <Input label="Data de Início" name="start_date" />
-          <Input label="Data de Termino" name="end_date" />
-          <TextArea label="Descrição" name="description" />
-          <div style={{ textAlign: "end" }}>
+          <div className="education__form--input">
+            <Input label="Instituição de ensino" name="institution" />
+          </div>
+          <div className="education__form--input">
+            <Input label="Cursos" name="course" />
+          </div>
+          <div className="education__form--input">
+            <Input label="Data de Início" name="start_date" />
+          </div>
+          <div className="education__form--input">
+            <Input label="Data de Termino" name="end_date" />
+          </div>
+          <div className="education__form--input">
+            <TextArea label="Descrição" name="description" />
+          </div>
+
+          <div className="education__form--action">
             <Button color="secondary" onClick={modal.close}>
               Cancelar
             </Button>
@@ -42,7 +89,3 @@ const EducationForm: React.FunctionComponent = () => {
 };
 
 export default EducationForm;
-
-function useFom() {
-  throw new Error("Function not implemented.");
-}
